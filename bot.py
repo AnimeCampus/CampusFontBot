5996a7e21a742c4efffeb135d5997eb02aa3f57e
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-10 17:15:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ if __name__ == "__main__" :
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH, 
-        bot=plugins,
+        plugins=bot,
         workers=100
     )
     app.run()
